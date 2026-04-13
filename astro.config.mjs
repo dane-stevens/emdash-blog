d46bd420bc74dcf18b63f3be60edc7f4d4356875
Dane Stevens <dane@dsmedia.ca>
2026-04-13 16:17:50 -0400

    split config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,29 @@ import node from "@astrojs/node";
 import react from "@astrojs/react";
 import { auditLogPlugin } from "@emdash-cms/plugin-audit-log";
 import { defineConfig, fontProviders } from "astro/config";
-import emdash, { s3 } from "emdash/astro";
-import { postgres } from "emdash/db";
+import emdash, { s3,local } from "emdash/astro";
+import { postgres, sqlite } from "emdash/db";
+
+const database= import.meta.env.PROD
+? postgres({
+				connectionString: process.env.DATABASE_URL
+			})
+			: sqlite({ url:"file:./data.db" })
+
+
+const storage = import.meta.env.PROD
+  ? s3({
+		endpoint: process.env.S3_ENDPOINT,
+		bucket: process.env.S3_BUCKET,
+		accessKeyId: process.env.S3_ACCESS_KEY_ID,
+		secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: "auto", // Optional, defaults to "auto"
+    // publicUrl: process.env.S3_PUBLIC_URL, // Optional CDN URL
+	})
+	: local({
+		directory:"./uploads",
+		baseUrl: "/_emdash/api/media/file"
+	})
 
 export default defineConfig({
 	output: "server",
@@ -17,17 +38,8 @@ export default defineConfig({
 	integrations: [
 		react(),
 		emdash({
-			database: postgres({
-				connectionString: process.env.DATABASE_URL
-			}),
-			storage: s3({
-				endpoint: process.env.S3_ENDPOINT,
-				bucket: process.env.S3_BUCKET,
-				accessKeyId: process.env.S3_ACCESS_KEY_ID,
-				secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-        region: "auto", // Optional, defaults to "auto"
-        // publicUrl: process.env.S3_PUBLIC_URL, // Optional CDN URL
-			}),
+			database,
+			storage,
 			plugins: [auditLogPlugin()],
 			siteUrl: process.env.SITE_URL
 		}),
